044917fb5b4ec5797b3e0f8d218baeacc913edc1
ragunath87 <ragucamera87@gmail.com>
2023-12-17 23:25:40 +0530

    changes

--- myweb/src/core/component/MainBody.tsx
+++ myweb/src/core/component/MainBody.tsx
@@ -1,16 +1,9 @@
-import { Col, Container, FloatingLabel, Form, Row, Stack } from "react-bootstrap";
-import Button from '../assetsui/Buttons';
-import Checkbox from "../assetsui/Checkbox";
-import { useState } from "react";
+import { Col, Container, Row} from "react-bootstrap";
 import LottieAnimation from '../animations/LottieAnimation';
 import animationData1 from '../animations/Peace.json';
 
 const MainBody = () => {
-    const [isChecked, setIsChecked] = useState(false);
 
-    const handleCheckboxChange = (checked: boolean) => {
-        setIsChecked(checked);
-    };
 
     return(
         <>
